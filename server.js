816405af083d8573aa2c2fdf0abe9e7dd765f49a
Iris <iris@MacBook-Pro-van-Iris.local>
2017-05-03 13:12:01 +0200

    test thingy

--- server.js
+++ server.js
@@ -78,6 +78,9 @@ app.get('/', function(req, res){
 // Listen on the connection event for incoming sockets, and log it to the console.
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 app.listen((process.env.PORT || 5000), function () {

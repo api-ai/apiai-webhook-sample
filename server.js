7d6e70a5c72cdcd6bcf628e936e784bf851679c5
Iris <iris@MacBook-Pro-van-Iris.local>
2017-05-04 11:00:25 +0200

    test message from server to client

--- server.js
+++ server.js
@@ -80,10 +80,15 @@ app.get('/', function(req, res){
 // Listen on the connection event for incoming sockets, and log it to the console.
 io.on('connection', function(socket){
   console.log('a user connected');
+  setTimeout(function(){
+    socket.send('Sent a message 4seconds after connection!');
+  }, 4000);
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
+
+
 
 http.listen((process.env.PORT || 5000), function () {
     console.log("Server started");
